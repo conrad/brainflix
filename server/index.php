4051d9684cbfd68b3588cb2f04bf679081d96aab
Matt Conrad <mmconrad@gmail.com>
2015-05-02 16:40:11 -0700

    Adds files for php deployment on Heroku AND initial firebase files

---
+++ server/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require('../vendor/autoload.php');
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Our web handlers
+
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return 'Hello';
+});
+
+$app->run();
+
+?>
